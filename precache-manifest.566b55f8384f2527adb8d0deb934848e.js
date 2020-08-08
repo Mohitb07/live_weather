81bf5acc44b215530fb0e5356fbda10b8a43b816
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-08-08 18:30:00 +0530

    Updates

---
+++ precache-manifest.566b55f8384f2527adb8d0deb934848e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fbcd9c1d50df5bd3b8247cce641f82",
+    "url": "/live_weather/index.html"
+  },
+  {
+    "revision": "ae2ff6cae3423dcf288f",
+    "url": "/live_weather/static/css/main.aa9613c8.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/live_weather/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/live_weather/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2ff6cae3423dcf288f",
+    "url": "/live_weather/static/js/main.374dff7c.chunk.js"
+  },
+  {
+    "revision": "489ede6083b63aa7e746",
+    "url": "/live_weather/static/js/runtime-main.7566b9ef.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/live_weather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "b623b0e4d80aee66fc86596185a3f0b7",
+    "url": "/live_weather/static/media/cold.b623b0e4.jpg"
+  },
+  {
+    "revision": "ff4e032fff334546068e281c93c51430",
+    "url": "/live_weather/static/media/summer.ff4e032f.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/live_weather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
