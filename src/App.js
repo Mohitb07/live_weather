ec8a7ee61e94c26e21071a88eb289aec261cc0fe
mohitb07 <bmohit980@gmail.com>
2022-10-09 22:05:51 +0530

    removed unused effect

--- src/App.js
+++ src/App.js
@@ -1,102 +1,155 @@
-import React, {useState } from 'react';
-import index from './index.css'
-
+import React, { useState } from "react";
+import "./index.css";
 
 const api = {
-    key : 'b267e1938cf3553846b90e9d9090a5ed',
-    base: 'https://api.openweathermap.org/data/2.5/'
-}
+  key: "b267e1938cf3553846b90e9d9090a5ed",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 const App = () => {
-
-
-    const [query, setQuery] = useState('');
-    const [weather, setWeather]= useState({});
-    const [dailyDataMax, setDailyDataMax] = useState([]);
-    const [dailyDataMin, setDailyDataMin] = useState([]);
-    const [dailyDataWeather, setDailyDataWeather] = useState([]);
-    
-
-    const search = evt => {
-        if (evt.key === "Enter"){
-            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-            .then(res => res.json())
-            .then(data => {
-                setWeather(data);
-                setQuery('');
-                
-                if (data.cod!=404){               
-                    fetch(`${api.base}onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&
-                    exclude=daily,&appid=${api.key}`)          
-                    .then(res => res.json())
-                    .then(data => {
-                    setDailyDataMax(data.daily[1].temp.max - 273.15);
-                    setDailyDataMin(data.daily[1].temp.min - 273.15);
-                    setDailyDataWeather(data.daily[1].weather[0].description);
-                })
-            }
-            })
-        }
+  const [query, setQuery] = useState();
+  const [weather, setWeather] = useState({});
+  const [dailyDataMax, setDailyDataMax] = useState([]);
+  const [dailyDataMin, setDailyDataMin] = useState([]);
+  const [dailyDataWeather, setDailyDataWeather] = useState([]);
+  const [error, setError] = useState(null);
+  // useEffect(() => {
+  //   window.navigator.geolocation.getCurrentPosition(
+  //     (pos) => {
+  //       let lat = pos.coords.latitude;
+  //       let long = pos.coords.longitude;
+  //       console.log(lat, long);
+  //       fetch(`${api.base}onecall?lat=${lat}&lon=${long}&
+  //                 exclude=daily,&appid=${api.key}`)
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //           console.log("data", data);
+  //           const dataSet =
+  //           setDailyDataMax(data.daily[1].temp.max - 273.15);
+  //           setDailyDataMin(data.daily[1].temp.min - 273.15);
+  //           setDailyDataWeather(data.daily[1].weather[0].description);
+  //         });
+  //     },
+  //     (err) => console.log("error", err)
+  //   );
+  // }, []);
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      setError("");
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setWeather(data);
+          console.log("outside data", data);
+          setQuery("");
+          if (data.cod === "404") {
+            setError(data.message);
+            return;
+          }
+          console.log("inside data", data);
+          fetch(`${api.base}onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&
+                    exclude=daily,&appid=${api.key}`)
+            .then((res) => res.json())
+            .then((data) => {
+              setDailyDataMax(data.daily[1].temp.max - 273.15);
+              setDailyDataMin(data.daily[1].temp.min - 273.15);
+              setDailyDataWeather(data.daily[1].weather[0].description);
+            });
+        })
+        .catch((err) => {
+          setError(err.message);
+        });
     }
+  };
 
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
 
-    const dateBuilder = (d) => {
-        let months = ["January", "February","March", "April","May", "June","July", "August","September", "October","November", "December"]
-        let days = ["Sunday", "Monday","Tuesday", "Wednesday","Thursday","Friday","Saturday", "Sunday"]
-    
-    
-        let day = days[d.getDay()];
-        let date = d.getDate();
-        let month = months[d.getMonth()];
-        let year = d.getFullYear();
-        
-        return `${day} ${date} ${month} ${year}`
-    }
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-    return (
-        <div className={(typeof weather.main!="undefined") ? 
-        ((weather.weather[0].main === 'Haze') ? 'haze':(weather.weather[0].main==='Clouds') ? 'clouds':(weather.weather[0].main ==='Rain')? 'rain':(weather.weather[0].main === 'Thunderstorm')?'thunderstorm':(weather.weather[0].main === 'Clear')?'clear':(weather.weather[0].main === 'Mist')?'mist':(weather.weather[0].main === 'Smoke')? 'smoke':'')
-        
-        
-        :'app'}>
+    return `${day} ${date} ${month} ${year}`;
+  };
 
-            <main>
-                <div className="search-box">
-                    <input
-                        type="text"
-                        className="search-bar"
-                        placeholder="Search"
-                        onChange={e => setQuery(e.target.value)}
-                        value={query}
-                        onKeyPress={search}
-                    />
-                </div>
-                {(typeof weather.main!="undefined") ?(
-                <div>
-                <div className="location-box">
-                <div className="location">{weather.name}, {weather.sys.country}</div>
-                    <div className="date">
-                        {dateBuilder(new Date())}
-                    </div>
-                </div>
-                <div className="weather-box">
-                <div className="temp">{Math.round(weather.main.temp)}&#176;C</div>
-                    <div className="weather">
-                        <div id="feels-like"> Feels like &nbsp;
-                        {Math.round(weather.main.feels_like)}&#176;
-                        </div>
-                        {weather.weather[0].main}
-                        <h6>Tomorrow</h6>
-                        <p>{dailyDataWeather}</p>
-                        <p>Max {Math.floor(dailyDataMax)}&#176;  / Min {Math.floor(dailyDataMin)}&#176;</p>
-
-                    </div>
-                </div>
-                </div>
-                ):('') }
-            </main>
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.weather[0].main.toLowerCase()
+          : "app"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-    )
-}
+        {error && (
+          <div className="weather-box">
+            <div className="error">{error}</div>
+          </div>
+        )}
+        {typeof weather.main != "undefined" && (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}&#176;C</div>
+              <div className="weather">
+                <div id="feels-like">
+                  {" "}
+                  Feels like &nbsp;
+                  {Math.round(weather.main.feels_like)}&#176;
+                </div>
+                {weather.weather[0].main}
+                <h6>Tomorrow</h6>
+                <p>{dailyDataWeather}</p>
+                <p>
+                  Max {Math.floor(dailyDataMax)}&#176; / Min{" "}
+                  {Math.floor(dailyDataMin)}&#176;
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </main>
+    </div>
+  );
+};
 
 export default App;
